70f1123ddfa9e9e18d161ae81c65416fc7d4503e
SofiiaTrokhymchuk <s.s.trokhymchuk@gmail.com>
2022-08-18 17:18:08 +0300

    Font Fix

--- js/createCharacter.js
+++ js/createCharacter.js
@@ -4,9 +4,9 @@ const navList = document.querySelector(".nav-list");
 const characterSection = document.querySelector(".character");
 const characterImg = document.createElement("img");
 characterImg.setAttribute("class", "character-img");
-const characterName = document.createElement("h2");
+const characterName = document.createElement("h2")
 characterName.setAttribute("class", "character-name");
-const characterDescription = document.createElement("p");
+const characterDescription = document.createElement("p")
 characterDescription.setAttribute("class", "character-description");
 
 
@@ -17,19 +17,10 @@ export function createCharacter(event){
     let navItem = event.target;
     let characterId = navItem.getAttribute("id");
     if(parseInt(characterId) === characters[characterId].id){
-        setActiveCharacter(event);
         characterImg.setAttribute("src", characters[characterId].image);
-        characterImg.setAttribute("alt", `${characters[characterId].name} Image`);
+        characterImg.setAttribute("alt", `${characters[characterId].name} Image`)
         characterName.innerHTML = characters[characterId].name;
         characterDescription.innerHTML = characters[characterId].description;
     }; 
-}
-
-function setActiveCharacter(event){
-    const activeCharacter = event.target;
-    if(document.querySelector(".nav-item .selected") !== null){
-        document.querySelector(".nav-item .selected").classList.remove("selected");
-        activeCharacter.classList.add("selected");
-    };
 }
 
